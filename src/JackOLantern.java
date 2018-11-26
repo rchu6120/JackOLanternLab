ab42031185fd58e4ad6ee87728a8f0f549683145
rchu9700 <rchu9700@bths.edu>
2018-11-26 14:45:45 -0500

    1st push

---
+++ src/JackOLantern.java
@@ -0,0 +1,19 @@
+public class JackOLantern {
+    private String[][] faceFeatures;
+
+    public JackOLantern(String[][] faceFeatures) {
+        this.faceFeatures = faceFeatures;
+    }
+
+    public void edit(String replace, int row, int column) {
+
+    }
+
+    public void fill(String str) {
+
+    }
+
+    public String toString() {
+
+    }
+}
--- src/JackOLanternRunner.java
+++ src/JackOLanternRunner.java
@@ -1,2 +1,9 @@
 public class JackOLanternRunner {
+    public static void main(String[] args) {
+        String[][] face = new String [5][8];
+        JackOLantern face1 = new JackOLantern(face);
+
+        face1.fill("X");
+        face1.edit("")
+    }
 }
