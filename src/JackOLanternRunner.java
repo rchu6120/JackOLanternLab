c9104d88f3d9ce87bafb00b7c3260d6331717064
Ricky <rchu9700@bths.edu>
2018-11-27 20:33:24 -0500

    done!

--- src/JackOLanternRunner.java
+++ src/JackOLanternRunner.java
@@ -8,19 +8,19 @@ public class JackOLanternRunner {
         face1.edit(" ",4,7);
         face1.edit(" ",0,7);
         face1.edit(" ",4,0);
-
         face1.edit("*", 1,2);
         face1.edit("*",1,5);
         face1.edit("^",2,3);
         face1.edit("^",2,4);
-        face1.edit("|",3,1);
-        face1.edit("|",3,2);
-        face1.edit("0",3,3);
-        face1.edit("0",3,4);
-        face1.edit("|",3,5);
-        face1.edit("|",3,6);
-        char c = (char)(0xa7fc);
-        face1.edit(c + "",4,2);
+        face1.edit("_",3,1);
+        face1.edit("_",3,2);
+        face1.edit(">",3,3);
+        face1.edit("<",3,4);
+        face1.edit("_",3,5);
+        face1.edit("_",3,6);
+        char c = (char)(0x02A0);
+        char a = (char)(0x01A4);
+        face1.edit(a + "",4,2);
         face1.edit(c + "",4,5);
         System.out.println(face1);
     }
